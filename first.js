658749cea049e178ecbb679db1aeb68bc51370a4
rakibhasan47 <mdrakib6585@gmail.com>
2023-01-15 07:36:41 +0600

    new jsvascript add

---
+++ first.js
@@ -0,0 +1,12 @@
+
+// VariableName ="Newmeric"
+var banana = 20;
+var mango = 80;
+
+// var VariableName = "String"
+var name = "rakib hasan";
+var yourName = "niloy ahmed";
+
+// var VariableName = "Boolean"
+var pass = true;
+var subscribed = false;
---
+++ mathematical-oparation.js
@@ -0,0 +1,6 @@
+var onionPrice = 43;
+var eggPrice = 50;
+// console.log(onionPrice);
+
+// var allPrice = onionPrice + eggPrice;
+console.log(onionPrice + eggPrice);
