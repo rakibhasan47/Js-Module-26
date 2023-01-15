7c46d5c8db003c2d6ad9d0d39d34d9a64c7332a8
rakibhasan47 <mdrakib6585@gmail.com>
2023-01-15 08:07:41 +0600

    update code

---
+++ conversion.js
@@ -0,0 +1,9 @@
+var firstName = 'Angel Alex';
+var lastName = 'Alex Fariha';
+var fullName = firstName+ ' ' + lastName;
+// console.log(fullName);
+
+var fast = 'Rakib';
+var last = 'Samia';
+var name = fast + ' ' + last;
+// console.log(name);
